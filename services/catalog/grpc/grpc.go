769c073c7542f823c07149ea64d730d057531d3e
dnakano <dnakano@student.42tokyo.jp>
2021-09-29 08:04:07 +0900

    add catalog service (but not works)

---
+++ services/catalog/grpc/grpc.go
@@ -0,0 +1,59 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	pkggrpc "github.com/dai65527/microservice-handson/pkg/grpc"
+	"github.com/dai65527/microservice-handson/services/catalog/proto"
+	customer "github.com/dai65527/microservice-handson/services/customer/proto"
+	item "github.com/dai65527/microservice-handson/services/item/proto"
+	"github.com/go-logr/logr"
+	"google.golang.org/grpc"
+)
+
+func RunServer(ctx context.Context, port int, logger logr.Logger) error {
+	opts := []grpc.DialOption{
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+		grpc.WithDefaultCallOptions(grpc.WaitForReady(true)),
+	}
+
+	itemHost := os.Getenv("ITEM_HOST")
+	if itemHost == "" {
+		itemHost = "localhost"
+	}
+	itemPort := os.Getenv("ITEM_PORT")
+	if itemPort == "" {
+		itemPort = "5003"
+	}
+	// item connection
+	iconn, err := grpc.DialContext(ctx, fmt.Sprintf("%s:%s", itemHost, itemPort), opts...)
+	if err != nil {
+		return fmt.Errorf("failed to dial item grpc server: %w", err)
+	}
+
+	customerHost := os.Getenv("CUSTOMER_HOST")
+	if customerHost == "" {
+		customerHost = "localhost"
+	}
+	customerPort := os.Getenv("CUSTOMER_PORT")
+	if customerPort == "" {
+		customerPort = "5002"
+	}
+	// customer connection
+	cconn, err := grpc.DialContext(ctx, fmt.Sprintf("%s:%s", customerHost, customerPort), opts...)
+	if err != nil {
+		return fmt.Errorf("failed to dial customer grpc server: %w", err)
+	}
+
+	svc := &server{
+		customerClient: customer.NewCustomerServiceClient(cconn),
+		itemClient:     item.NewItemServiceClient(iconn),
+	}
+
+	return pkggrpc.NewServer(port, logger, func(s *grpc.Server) {
+		proto.RegisterCatalogServiceServer(s, svc)
+	}).Start(ctx)
+}
---
+++ services/catalog/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+
+	"github.com/dai65527/microservice-handson/pkg/logger"
+	"github.com/dai65527/microservice-handson/platform/db/grpc"
+	"golang.org/x/sys/unix"
+)
+
+func main() {
+	os.Exit(run(context.Background()))
+}
+
+func run(ctx context.Context) int {
+	ctx, stop := signal.NotifyContext(ctx, unix.SIGTERM, unix.SIGINT)
+	defer stop()
+
+	l, err := logger.New()
+	if err != nil {
+		_, ferr := fmt.Fprintf(os.Stderr, "failed to create logger: %s", err)
+		if ferr != nil {
+			// Unhandleable, something went wrong...
+			panic(fmt.Sprintf("failed to write log:`%s` original error is:`%s`", ferr, err))
+		}
+		return 1
+	}
+	clogger := l.WithName("catalog")
+
+	port, err := strconv.Atoi(os.Getenv("LISTEN_PORT"))
+	if err != nil {
+		port = 5004
+	}
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- grpc.RunServer(ctx, port, clogger.WithName("grpc"))
+	}()
+
+	select {
+	case err := <-errCh:
+		fmt.Println(err.Error())
+		return 1
+	case <-ctx.Done():
+		fmt.Println("shutting down...")
+		return 0
+	}
+}
